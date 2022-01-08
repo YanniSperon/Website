d833cce1f7832fae5fc619de8b49d3595953cae6
Yanni Speron <47045424+YanniSperon@users.noreply.github.com>
2022-01-07 18:16:08 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
+function onWindowLoad() {
+	changeGearContainerSize();
+}
 
+function changeGearContainerSize() {
+	document.getElementById("gearcontainer").style.height = getWidth(document.getElementById("gearcontainer")) + "px";
+}
+
+window.onresize = changeGearContainerSize;
+
+function getWidth(element) {
+	return parseFloat(window.getComputedStyle(element).width);
+}
+
+function homeHeaderClicked() {
+	//this.location.href = '#'
+	this.location.reload();
+}
+
+function workHeaderClicked() {
+	//this.location.href = '#'
+	this.location.reload();
+}
+
+function contactHeaderClicked() {
+	//this.location.href = '#'
+	this.location.reload();
+}
